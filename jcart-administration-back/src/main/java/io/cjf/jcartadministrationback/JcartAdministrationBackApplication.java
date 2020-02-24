ebe38b3bc21e26068d491498dd2b93b63aa78669
chenjiefei <nlpx_dc@hotmail.com>
2020-02-24 11:50:47 +0800

    first init jcart administration back

---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/JcartAdministrationBackApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jcartadministrationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartAdministrationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartAdministrationBackApplication.class, args);
+    }
+
+}
---
+++ jcart-administration-back/src/test/java/io/cjf/jcartadministrationback/JcartAdministrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jcartadministrationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartAdministrationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
