52548eb31f24662bf3996eb030ba8954dd2bb4f3
chenjiefei <nlpx_dc@hotmail.com>
2020-02-24 11:52:06 +0800

    first init jcart store back

---
+++ jcart-store-back/src/main/java/io/cjf/jcartstoreback/JcartStoreBackApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jcartstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartStoreBackApplication.class, args);
+    }
+
+}
---
+++ jcart-store-back/src/test/java/io/cjf/jcartstoreback/JcartStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jcartstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
