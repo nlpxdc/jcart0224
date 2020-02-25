48a7c3e611f83ca5bb1754c8004375bc19726d5a
chenjiefei <nlpx_dc@hotmail.com>
2020-02-25 11:30:14 +0800

    draft administration administrator delete apis

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
@@ -7,6 +7,8 @@ import io.cjf.jcartadministrationback.dto.out.AdministratorShowOutDTO;
 import io.cjf.jcartadministrationback.dto.out.PageOutDTO;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/administrator")
 public class AdministratorController {
@@ -54,6 +56,16 @@ public class AdministratorController {
     @PostMapping("/update")
     public void update(@RequestBody AdministratorUpdateInDTO administratorUpdateInDTO){
 
+    }
+
+    @PostMapping("/delete")
+    public void delete(@RequestBody Integer adminstratorId){
+
+    }
+
+    @PostMapping("/batchDelete")
+    public void batchDelete(@RequestBody List<Integer> administratorIds){
+        
     }
 
 }
