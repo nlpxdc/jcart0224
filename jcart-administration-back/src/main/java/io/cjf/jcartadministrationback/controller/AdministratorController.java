a59f74577c642a4597021cc2ae729bc0926e16b9
chenjiefei <nlpx_dc@hotmail.com>
2020-02-25 09:37:51 +0800

    add some administrator apis

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package io.cjf.jcartadministrationback.controller;
 
 import io.cjf.jcartadministrationback.dto.in.AdministratorLoginInDTO;
+import io.cjf.jcartadministrationback.dto.in.AdministratorResetPwdInDTO;
 import io.cjf.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
 import io.cjf.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
+import io.cjf.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.cjf.jcartadministrationback.dto.out.PageOutDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,6 +25,21 @@ public class AdministratorController {
     @PostMapping("/updateProfile")
     public void updateProfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
 
+    }
+
+    @GetMapping("/getPwdResetCode")
+    public String getPwdResetCode(@RequestParam String email){
+        return null;
+    }
+
+    @PostMapping("/resetPwd")
+    public void resetPwd(@RequestBody AdministratorResetPwdInDTO administratorResetPwdInDTO){
+
+    }
+
+    @GetMapping("/getList")
+    public PageOutDTO<AdministratorListOutDTO> getList(@RequestParam Integer pageNum){
+        return null;
     }
 
 }
---
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/dto/out/AdministratorListOutDTO.java
@@ -0,0 +1,40 @@
+package io.cjf.jcartadministrationback.dto.out;
+
+public class AdministratorListOutDTO {
+    private Integer administratorId;
+    private String username;
+    private Byte status;
+    private Long createTimestamp;
+
+    public Integer getAdministratorId() {
+        return administratorId;
+    }
+
+    public void setAdministratorId(Integer administratorId) {
+        this.administratorId = administratorId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Long getCreateTimestamp() {
+        return createTimestamp;
+    }
+
+    public void setCreateTimestamp(Long createTimestamp) {
+        this.createTimestamp = createTimestamp;
+    }
+}
