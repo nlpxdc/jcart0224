f626dbe58bbdb1735d44035ec8378ae519387580
chenjiefei <nlpx_dc@hotmail.com>
2020-02-27 11:38:26 +0800

    add administration product update page

---
+++ jcart-administration-front/viewmodels/product-update.js
@@ -0,0 +1,153 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        productId: '',
+        productCode: '',
+        productName: '',
+        price: '',
+        discount: '',
+        stockQuantity: '',
+        rewordPoints: '',
+        sortOrder: '',
+        description: '',
+        selectedStatus: 1,
+        selectedMainPic: '',
+        mainPicUrl: '',
+        selectedOtherPics: [],
+        otherPicUrls: [],
+        statuses: [
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ],
+        mainFileList: [],
+        otherFileList: []
+    },
+    mounted() {
+        console.log('view mounted');
+
+        var url = new URL(location.href);
+        this.productId = url.searchParams.get("productId");
+        if (!this.productId) {
+            alert('productId is null');
+            return;
+        }
+
+        this.getProductById();
+    },
+    methods: {
+        handleUpdateClick() {
+            console.log('update click');
+            this.updateProduct();
+        },
+        handleOnMainChange(val) {
+            this.selectedMainPic = val.raw;
+        },
+        handleUploadMainClick() {
+            console.log('upload main pic click');
+            this.uploadMainImage();
+        },
+        uploadMainImage() {
+            var formData = new FormData();
+            formData.append("image", this.selectedMainPic);
+
+            axios.post('/image/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.mainPicUrl = response.data;
+                    alert('上传成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    alert('上传失败');
+                });
+        },
+        handleOnOtherChange(file, fileList) {
+            console.log('fileList', fileList);
+            this.selectedOtherPics = fileList;
+        },
+        handleOnOtherRemove(file, fileList) {
+            console.log('fileList', fileList);
+            this.selectedOtherPics = fileList;
+        },
+        handleUploadOtherClick() {
+            console.log('upload other pic click');
+            this.uploadOtherImage();
+        },
+        uploadOtherImage() {
+            this.selectedOtherPics.forEach(pic => {
+                var formData = new FormData();
+                formData.append("image", pic.raw);
+
+                axios.post('/image/upload', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                    .then(function (response) {
+                        console.log(response);
+                        var url = response.data;
+                        app.otherPicUrls.push(url);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                        alert('上床失败');
+                    });
+            });
+
+
+        },
+        updateProduct() {
+            axios.post('/product/update', {
+                productId: this.productId,
+                productName: this.productName,
+                price: this.price,
+                discount: this.discount,
+                stockQuantity: this.stockQuantity,
+                status: this.selectedStatus,
+                mainPicUrl: this.mainPicUrl,
+                rewordPoints: this.rewordPoints,
+                sortOrder: this.sortOrder,
+                description: this.description,
+                otherPicUrls: this.otherPicUrls
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('修改成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        getProductById() {
+            axios.get('/product/getById', {
+                params: {
+                    productId: this.productId
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    var product = response.data;
+                    app.productId = product.productId;
+                    app.productCode = product.productCode;
+                    app.productName = product.productName;
+                    app.price = product.price;
+                    app.discount = product.discount;
+                    app.stockQuantity = product.stockQuantity;
+                    app.selectedStatus = product.status;
+                    app.rewordPoints = product.rewordPoints;
+                    app.sortOrder = product.sortOrder;
+                    app.mainPicUrl = product.mainPicUrl;
+                    app.description = product.description;
+                    app.otherPicUrls = product.otherPicUrls;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+})
