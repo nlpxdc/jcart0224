9e0da406fa929cf62cf03c1361aede8e07e59685
chenjiefei <nlpx_dc@hotmail.com>
2020-02-27 14:29:14 +0800

    add store product show page

---
+++ jcart-store-front/viewmodels/product-show.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        productId: '',
+        productCode: '',
+        productName: '',
+        price: '',
+        discount: '',
+        description: '',
+        stockQuantity: '',
+        mainPicUrl: '',
+        otherPicUrls: ''
+    },
+    mounted(){
+        console.log('view mounted');
+
+        var url = new URL(location.href);
+        this.productId = url.searchParams.get("productId");
+        if (!this.productId) {
+            alert('productId is null');
+            return;
+        }
+
+        this.getProductById();
+    },
+    methods: {
+        getProductById() {
+            axios.get('/product/getById', {
+                params: {
+                    productId: this.productId
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    var product = response.data;
+                    app.productId = product.productId;
+                    app.productCode = product.productCode;
+                    app.productName = product.productName;
+                    app.price = product.price;
+                    app.discount = product.discount;
+                    app.mainPicUrl = product.mainPicUrl;
+                    app.otherPicUrls = product.otherPicUrls;
+                    app.description = product.description;
+                    app.stockQuantity = product.stockQuantity;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+})
