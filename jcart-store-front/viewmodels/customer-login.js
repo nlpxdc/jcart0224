240750429a0e85e5e1bdb50f0b7c7d7c79619f7d
chenjiefei <nlpx_dc@hotmail.com>
2020-02-28 14:28:31 +0800

    add store customer login page

---
+++ jcart-store-front/viewmodels/customer-login.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        username: '',
+        password: ''
+    },
+    methods: {
+        handleLoginClick() {
+            console.log('login click');
+            this.loginCustomer();
+        },
+        loginCustomer() {
+            axios.get('/customer/login', {
+                params: {
+                    username: this.username,
+                    password: this.password
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    var dto = response.data;
+                    console.log(dto);
+                    alert('登陆成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+})
