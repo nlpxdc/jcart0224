2c9e8cd6cf8d168a83df9c018098dafd28601ff4
chenjiefei <nlpx_dc@hotmail.com>
2020-03-02 12:09:38 +0800

    add administration administrator batch delete page

--- jcart-administration-front/viewmodels/administrator-index.js
+++ jcart-administration-front/viewmodels/administrator-index.js
@@ -2,7 +2,13 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        selectedAdministrators: []
+    },
+    computed: {
+        selectedAdministratorIds() {
+            return this.selectedAdministrators.map(a => a.administratorId);
+        }
     },
     mounted() {
         console.log('view mounted');
@@ -21,6 +27,16 @@ var app = new Vue({
                 this.deleteAdministrator(row.administratorId);
             }
         },
+        handleBatchDeleteClick() {
+            console.log('batch delete click');
+            if (confirm("确认删除？")) {
+                this.batchDeleteAdministrators();
+            }
+        },
+        handleSelectionChange(val) {
+            console.log('selection change', val);
+            this.selectedAdministrators = val;
+        },
         deleteAdministrator(administratorId) {
             axios.post('/administrator/delete', administratorId, {
                 headers: {
@@ -30,6 +46,17 @@ var app = new Vue({
                 .then(function (response) {
                     console.log(response);
                     alert('删除成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        batchDeleteAdministrators() {
+            axios.post('/administrator/batchDelete', this.selectedAdministratorIds)
+                .then(function (response) {
+                    console.log(response);
+                    alert('批删成功');
                     location.reload();
                 })
                 .catch(function (error) {
