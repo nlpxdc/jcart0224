88c9d8a0917b1156c309ac3a792e25a45ee5b686
chenjiefei <nlpx_dc@hotmail.com>
2020-03-02 11:56:22 +0800

    add administration administrator delete page

--- jcart-administration-front/viewmodels/administrator-index.js
+++ jcart-administration-front/viewmodels/administrator-index.js
@@ -4,15 +4,37 @@ var app = new Vue({
         pageInfo: '',
         pageNum: 1
     },
-    mounted(){
+    mounted() {
         console.log('view mounted');
         this.getAdministrators();
     },
     methods: {
-        handlePageChange(val){
+        handlePageChange(val) {
             console.log('page change', val);
             this.pageNum = val;
             this.getAdministrators();
+        },
+        handleDelete(index, row) {
+            console.log('delete click');
+
+            if (confirm("确认删除？")) {
+                this.deleteAdministrator(row.administratorId);
+            }
+        },
+        deleteAdministrator(administratorId) {
+            axios.post('/administrator/delete', administratorId, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('删除成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         },
         getAdministrators() {
             axios.get('/administrator/getList', {
