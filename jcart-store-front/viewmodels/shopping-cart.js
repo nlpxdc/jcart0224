9ee3d21ae68afaf9b020f9b5aa7631d4cbff57e2
chenjiefei <nlpx_dc@hotmail.com>
2020-03-03 15:29:31 +0800

    fix shopping cart issue with total price

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -3,6 +3,24 @@ var app = new Vue({
     data: {
         myShoppingCart: []
     },
+    computed: {
+        totalPrice() {
+            var subTotalPrices = this.myShoppingCart.map(p => {
+                return p.unitPrice * p.quantity;
+            });
+            var totalPrice = subTotalPrices.reduce((a, b) => a + b, 0);
+            var totalPriceStr = totalPrice.toFixed(2);
+            totalPrice = parseFloat(totalPriceStr);
+            return totalPrice;
+        }
+    },
+    // watch: {
+    //     totalPrice: function (newVal, oldVal) {
+    //         console.log('my total price change', newVal);
+    //         localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+    //         this.$message.success('修改购物车成功');
+    //     }
+    // },
     mounted() {
         console.log('view mounted');
         var myShoppingCartJson = localStorage['myShoppingCartJson'];
