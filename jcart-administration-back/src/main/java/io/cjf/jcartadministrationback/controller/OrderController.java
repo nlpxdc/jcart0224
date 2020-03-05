dee223bcdc3077b0d36789275f45ed91b5d55832
chenjiefei <nlpx_dc@hotmail.com>
2020-03-05 13:57:22 +0800

    add administration order search page

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/OrderController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/OrderController.java
@@ -5,13 +5,11 @@ import io.cjf.jcartadministrationback.dto.in.OrderSearchInDTO;
 import io.cjf.jcartadministrationback.dto.out.*;
 import io.cjf.jcartadministrationback.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin
 public class OrderController {
 
     @Autowired
---
+++ jcart-administration-front/viewmodels/order-search.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        pageInfo: '',
+        pageNum: 1
+    },
+    mounted() {
+        console.log('view mounted');
+        this.searchOrder();
+    },
+    methods: {
+        handlePageChange() {
+            console.log('page changed', val);
+            this.pageNum = val;
+            this.searchOrder();
+        },
+        searchOrder() {
+            axios.get('/order/search', {
+                params: {
+                    pageNum: this.pageNum
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+})
