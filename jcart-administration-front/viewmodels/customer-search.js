5e055576d186970d9cee6e5108a1f298f15da1e9
chenjiefei <nlpx_dc@hotmail.com>
2020-03-05 12:05:25 +0800

    add administration customer update status page

--- jcart-administration-front/viewmodels/customer-search.js
+++ jcart-administration-front/viewmodels/customer-search.js
@@ -2,17 +2,26 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        statuses: [
+            { value: 0, label: '禁用' },
+            { value: 1, label: '启用' },
+            { value: 2, label: '不安全' }
+        ]
     },
     mounted() {
         console.log('view mounted');
         this.searchCustomer();
     },
     methods: {
-        handlePageChange(val){
+        handlePageChange(val) {
             console.log('page changed');
             this.pageNum = val;
             this.searchCustomer();
+        },
+        handleUpdateStatus(index, row) {
+            console.log('update status click');
+            this.updateCustomerStatus(row.customerId, row.status);
         },
         searchCustomer() {
             axios.get('/customer/search', {
@@ -23,6 +32,19 @@ var app = new Vue({
                 .then(function (response) {
                     console.log(response);
                     app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        updateCustomerStatus(customerId, status) {
+            axios.post('/customer/setStatus', {
+                customerId: customerId,
+                status: status
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('状态更新成功');
                 })
                 .catch(function (error) {
                     console.log(error);
