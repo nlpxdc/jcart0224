50826c52111c2a5b9c82baa5d01b28a297ffacab
chenjiefei <nlpx_dc@hotmail.com>
2020-03-05 13:13:42 +0800

    fix server port

--- jcart-store-front/assets/js/common.js
+++ jcart-store-front/assets/js/common.js
@@ -1,3 +1,3 @@
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://localhost:9090';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.common['jcartToken'] = localStorage['jcartToken'];
