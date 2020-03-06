2c1c2f54d3bb8795dd3aa7e6d0b15c161100798e
chenjiefei <nlpx_dc@hotmail.com>
2020-03-06 14:45:07 +0800

    fix administration return history create api

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/impl/ReturnHistoryServiceImpl.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/service/impl/ReturnHistoryServiceImpl.java
@@ -1,11 +1,15 @@
 package io.cjf.jcartadministrationback.service.impl;
 
 import io.cjf.jcartadministrationback.dao.ReturnHistoryMapper;
+import io.cjf.jcartadministrationback.po.Return;
 import io.cjf.jcartadministrationback.po.ReturnHistory;
 import io.cjf.jcartadministrationback.service.ReturnHistoryService;
+import io.cjf.jcartadministrationback.service.ReturnService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,6 +18,9 @@ public class ReturnHistoryServiceImpl implements ReturnHistoryService {
     @Autowired
     private ReturnHistoryMapper returnHistoryMapper;
 
+    @Autowired
+    private ReturnService returnService;
+
     @Override
     public List<ReturnHistory> getListByReturnId(Integer returnId) {
         List<ReturnHistory> returnHistories = returnHistoryMapper.selectListByReturnId(returnId);
@@ -21,9 +28,16 @@ public class ReturnHistoryServiceImpl implements ReturnHistoryService {
     }
 
     @Override
+    @Transactional
     public Long create(ReturnHistory returnHistory) {
         returnHistoryMapper.insertSelective(returnHistory);
         Long returnHistoryId = returnHistory.getReturnHistoryId();
+
+        Return aReturn = new Return();
+        aReturn.setReturnId(returnHistory.getReturnId());
+        aReturn.setUpdateTime(new Date());
+        returnService.update(aReturn);
+
         return returnHistoryId;
     }
 }
