b65d8ed0ae8a8c4934d00bd5f2360722dfed33a4
chenjiefei <nlpx_dc@hotmail.com>
2020-03-06 11:30:59 +0800

    add store return index page

---
+++ jcart-store-front/viewmodels/return-index.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        pageInfo: '',
+        pageNum: 1
+    },
+    mounted() {
+        console.log('view mounted');
+        this.getMyReturn();
+    },
+    methods: {
+        handlePageChange(val) {
+            console.log('page changed', val);
+            this.pageNum = val;
+        },
+        getMyReturn() {
+            axios.get('/return/getList', {
+                params: {
+                    pageNum: this.pageNum
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+})
