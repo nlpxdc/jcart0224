d354c431d40f569f33880a8b4de4424e43cd8ccb
chenjiefei <nlpx_dc@hotmail.com>
2020-03-09 11:00:05 +0800

    add administration product search page

--- jcart-administration-front/viewmodels/product-search.js
+++ jcart-administration-front/viewmodels/product-search.js
@@ -2,14 +2,38 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        productCode: '',
+        productName: '',
+        price: '',
+        stockQuantity: '',
+        selectedStatus: '',
+        statuses: [
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ]
+
     },
     mounted() {
         console.log('view mounted');
         this.searchProduct();
     },
     methods: {
-        handlePageChange(val){
+        handleSearchClick() {
+            console.log('search click');
+            this.pageNum = 1;
+            this.searchProduct();
+        },
+        handleClearClick() {
+            console.log('clear click');
+            this.productCode = '';
+            this.productName = '';
+            this.price = '';
+            this.stockQuantity = '';
+            this.selectedStatus = '';
+        },
+        handlePageChange(val) {
             console.log('page change');
             this.pageNum = val;
             this.searchProduct();
@@ -17,6 +41,11 @@ var app = new Vue({
         searchProduct() {
             axios.get('/product/search', {
                 params: {
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    price: this.price,
+                    stockQuantity: this.stockQuantity,
+                    status: this.selectedStatus,
                     pageNum: this.pageNum
                 }
             })
