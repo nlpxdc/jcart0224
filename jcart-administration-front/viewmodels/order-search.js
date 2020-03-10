e78fb5658d761f1f4f24c5943a1eb63c5edc1630
chenjiefei <nlpx_dc@hotmail.com>
2020-03-10 10:15:44 +0800

    add administration order search page

--- jcart-administration-front/viewmodels/order-search.js
+++ jcart-administration-front/viewmodels/order-search.js
@@ -2,6 +2,27 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
+        orderId: '',
+        customerName: '',
+        totalPrice: '',
+        selectedStatus: '',
+        statuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '处理中' },
+            { value: 2, label: '待发货' },
+            { value: 3, label: '已发货' },
+            { value: 4, label: '待签收' },
+            { value: 5, label: '已签收' },
+            { value: 6, label: '待支付' },
+            { value: 7, label: '已支付' },
+            { value: 8, label: '取消' },
+            { value: 9, label: '拒绝' },
+            { value: 10, label: '完成' },
+            { value: 11, label: '待评价' },
+            { value: 12, label: '已评价' }
+        ],
+        startTime: '',
+        endTime: '',
         pageNum: 1
     },
     mounted() {
@@ -9,6 +30,20 @@ var app = new Vue({
         this.searchOrder();
     },
     methods: {
+        handleSearchClick() {
+            console.log('search click');
+            this.pageNum = 1;
+            this.searchOrder();
+        },
+        handleClearClick() {
+            console.log('clear click');
+            this.orderId = '';
+            this.customerName = '';
+            this.selectedStatus = '';
+            this.totalPrice = '';
+            this.startTime = '';
+            this.endTime = '';
+        },
         handlePageChange() {
             console.log('page changed', val);
             this.pageNum = val;
@@ -17,6 +52,12 @@ var app = new Vue({
         searchOrder() {
             axios.get('/order/search', {
                 params: {
+                    orderId: this.orderId,
+                    customerName: this.customerName,
+                    status: this.selectedStatus,
+                    totalPrice: this.totalPrice,
+                    startTimestamp: this.startTime ? this.startTime.getTime() : '',
+                    endTimestamp: this.endTime ? this.endTime.getTime() : '',
                     pageNum: this.pageNum
                 }
             })
