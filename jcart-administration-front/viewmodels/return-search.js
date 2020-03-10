bac553ac33795c72ebc016a3b1b868f1abd1bab1
chenjiefei <nlpx_dc@hotmail.com>
2020-03-10 10:53:10 +0800

    add administration return search page

--- jcart-administration-front/viewmodels/return-search.js
+++ jcart-administration-front/viewmodels/return-search.js
@@ -2,6 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
+        returnId: '',
+        orderId: '',
+        customerName: '',
+        productCode: '',
+        productName: '',
+        selectedStatus: '',
+        statuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ],
+        startTime: '',
+        endTime: '',
         pageNum: 1
     },
     mounted() {
@@ -9,6 +24,22 @@ var app = new Vue({
         this.searchReturn();
     },
     methods: {
+        handleSearchClick() {
+            console.log('search click');
+            this.pageNum = 1;
+            this.searchReturn();
+        },
+        handleClearClick() {
+            console.log('clear click');
+            this.returnId = '';
+            this.orderId = '';
+            this.customerName = '';
+            this.productCode = '';
+            this.productName = '';
+            this.selectedStatus = '';
+            this.startTime = '';
+            this.endTime = '';
+        },
         handlePageChange(val) {
             console.log('page changed', val);
             this.pageNum = val;
@@ -17,6 +48,14 @@ var app = new Vue({
         searchReturn() {
             axios.get('/return/search', {
                 params: {
+                    returnId: this.returnId,
+                    orderId: this.orderId,
+                    customerName: this.customerName,
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    status: this.selectedStatus,
+                    startTimestamp: this.startTime ? this.startTime.getTime() : '',
+                    endTimestamp: this.endTime ? this.endTime.getTime() : '',
                     pageNum: this.pageNum
                 }
             })
