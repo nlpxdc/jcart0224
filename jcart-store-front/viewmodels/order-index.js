b3174e18d1a9c99e3d0c61b133dba2d458fbdb69
chenjiefei <nlpx_dc@hotmail.com>
2020-03-11 10:41:47 +0800

    fix page

--- jcart-store-front/viewmodels/order-index.js
+++ jcart-store-front/viewmodels/order-index.js
@@ -2,14 +2,29 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        statuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '处理中' },
+            { value: 2, label: '待发货' },
+            { value: 3, label: '已发货' },
+            { value: 4, label: '待签收' },
+            { value: 5, label: '已签收' },
+            { value: 6, label: '待支付' },
+            { value: 7, label: '已支付' },
+            { value: 8, label: '取消' },
+            { value: 9, label: '拒绝' },
+            { value: 10, label: '完成' },
+            { value: 11, label: '待评价' },
+            { value: 12, label: '已评价' }
+        ]
     },
-    mounted(){
+    mounted() {
         console.log('view mounted');
         this.getMyOrders();
     },
     methods: {
-        handlePageChange(val){
+        handlePageChange(val) {
             console.log('page changed', val);
             this.pageNum = val;
             this.getMyOrders();
