b63ce571dec0f56d40ee7fa0ff79efbcebdd1fd3
chenjiefei <nlpx_dc@hotmail.com>
2020-03-11 11:23:21 +0800

    fix page

--- jcart-store-front/viewmodels/return-index.js
+++ jcart-store-front/viewmodels/return-index.js
@@ -2,7 +2,14 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        statuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ]
     },
     mounted() {
         console.log('view mounted');
