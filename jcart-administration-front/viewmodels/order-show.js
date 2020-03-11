8105a72053387482871372c6f8ac83e9d74f92f0
chenjiefei <nlpx_dc@hotmail.com>
2020-03-11 11:04:22 +0800

    fix page

--- jcart-administration-front/viewmodels/order-show.js
+++ jcart-administration-front/viewmodels/order-show.js
@@ -33,6 +33,20 @@ var app = new Vue({
             { value: 11, label: '待评价' },
             { value: 12, label: '已评价' }
         ],
+        shipMethods: [
+            { value: 0, label: 'EMS' },
+            { value: 1, label: '顺丰' },
+            { value: 2, label: '圆通' },
+            { value: 3, label: '中通' },
+            { value: 4, label: '申通' }
+        ],
+        payMethods: [
+            { value: 0, label: '货到付款' },
+            { value: 1, label: '借记卡' },
+            { value: 2, label: '信用卡' },
+            { value: 3, label: '微信支付' },
+            { value: 4, label: '支付宝' }
+        ],
         createHistoryOrderStatus: '',
         createHistoryCustomerNotified: false,
         createHistoryComment: ''
