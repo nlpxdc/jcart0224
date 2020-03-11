d35aa17c9cfdc7dc5d07ba76bb14906b67892ac2
chenjiefei <nlpx_dc@hotmail.com>
2020-03-11 11:53:13 +0800

    fix page

--- jcart-administration-front/viewmodels/returnhistory-index.js
+++ jcart-administration-front/viewmodels/returnhistory-index.js
@@ -5,6 +5,7 @@ var app = new Vue({
         returnHistories: [],
         selectedReturnStatus: '',
         returnStatuses: [
+            { value: 0, label: '待处理' },
             { value: 1, label: '待取货' },
             { value: 2, label: '正在处理' },
             { value: 3, label: '完成' },
