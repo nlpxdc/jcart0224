16c4ecf7d95e7a4be523f860b3e7b4517fe9fc7c
chenjiefei <nlpx_dc@hotmail.com>
2020-03-12 09:36:49 +0800

    fix administration product image show

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/ImageController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/ImageController.java
@@ -2,6 +2,7 @@ package io.cjf.jcartadministrationback.controller;
 
 import io.cjf.jcartadministrationback.constant.ClientExceptionConstant;
 import io.cjf.jcartadministrationback.exception.ClientException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +16,9 @@ import java.util.UUID;
 @RequestMapping("/image")
 @CrossOrigin
 public class ImageController {
+
+    @Value("${www.image.baseurl}")
+    private String imageBaseurl;
 
     private List<String> imageExts= Arrays.asList("jpg","jpeg","png");
 
@@ -36,7 +40,7 @@ public class ImageController {
             byte[] data = image.getBytes();
             out.write(data);
         }
-        return filename;
+        return imageBaseurl + "/" + filename;
     }
 
 }
