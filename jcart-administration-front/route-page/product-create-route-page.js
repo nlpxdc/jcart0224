343e38c101ba669975eac43dbd90ee068074521b
chenjiefei <nlpx_dc@hotmail.com>
2020-03-14 18:44:57 +0800

    fix page

--- jcart-administration-front/route-page/product-create-route-page.js
+++ jcart-administration-front/route-page/product-create-route-page.js
@@ -1,59 +1,60 @@
 const ProductCreateRoutePage = {
     template: `
     <div id="app">
-        <el-page-header @back="handleGoBack" content="添加商品">
-        </el-page-header>
-
-        <el-input v-model="productCode" placeholder="请输入商品代码"></el-input>
-        <el-input v-model="productName" placeholder="请输入商品名称"></el-input>
-        <el-input v-model="price" placeholder="请输入价格"></el-input>
-        <el-input v-model="discount" placeholder="请输入打折"></el-input>
-        <el-input v-model="stockQuantity" placeholder="请输入库存量"></el-input>
-        <el-input v-model="rewordPoints" placeholder="请输入积分"></el-input>
-        <el-input v-model="sortOrder" placeholder="请输入排序"></el-input>
-        <el-input v-model="productAbstract" type="textarea" placeholder="请输入摘要"></el-input>
-        <textarea id="mytextarea">{{description}}</textarea>
-
-        <el-select v-model="selectedStatus" placeholder="请选择状态">
-            <el-option v-for="item in statuses" :key="item.value" :label="item.label" :value="item.value">
-            </el-option>
-        </el-select>
-
-        <el-upload ref="uploadMain" action="" :http-request="uploadMainImage" :limit="1" accept="image/*"
-            :auto-upload="false" :on-change="handleOnMainChange" :file-list="mainFileList">
-            <el-button slot="trigger" size="small" type="primary">选取主图</el-button>
-            <el-button size="small" type="success" @click="handleUploadMainClick">上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件</div>
-        </el-upload>
-        上传后主图：<el-link type="primary">{{mainPicUrl}}</el-link><br>
-        <el-image style="width: 100px; height: 100px" :src="mainPicUrl" :fit="fit"></el-image>
-        <br>
-        <br>
-
-        <el-upload ref="uploadOther" multiple action="" :http-request="uploadOtherImage" :limit="9" accept="image/*"
-            :auto-upload="false" :on-change="handleOnOtherChange" :on-remove="handleOnOtherRemove"
-            :file-list="otherFileList">
-            <el-button slot="trigger" size="small" type="primary">选取其他图片</el-button>
-            <el-button size="small" type="success" @click="handleUploadOtherClick">上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件</div>
-        </el-upload>
-
-        <br>
-        <div v-for="item in otherPicUrls">
-            上传后其他图片：
-            <el-link type="primary">{{item}}</el-link><br>
-            <el-image style="width: 60px; height: 60px" :src="item" :fit="fit"></el-image>
+    <el-page-header @back="handleGoBack" content="添加商品">
+    </el-page-header>
+    <el-button type="primary" @click="handleCreateClick">创建</el-button>
+    <el-tabs v-model="activeTab" @tab-click="handleTabClick">
+        <el-tab-pane label="基本信息" name="generalTab">
+            <el-input v-model="productCode" placeholder="请输入商品代码"></el-input>
+            <el-input v-model="productName" placeholder="请输入商品名称"></el-input>
+            <el-input v-model="price" placeholder="请输入价格"></el-input>
+            <el-input v-model="discount" placeholder="请输入打折"></el-input>
+            <el-input v-model="stockQuantity" placeholder="请输入库存量"></el-input>
+            <el-input v-model="rewordPoints" placeholder="请输入积分"></el-input>
+            <el-input v-model="sortOrder" placeholder="请输入排序"></el-input>
+            <el-input v-model="productAbstract" type="textarea" placeholder="请输入摘要"></el-input>
+            <textarea id="mytextarea">{{description}}</textarea>
+    
+            <el-select v-model="selectedStatus" placeholder="请选择状态">
+                <el-option v-for="item in statuses" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+            </el-select>
+        </el-tab-pane>
+        <el-tab-pane label="图片" name="imageTab">
+            <el-upload ref="uploadMain" action="" :http-request="uploadMainImage" :limit="1" accept="image/*"
+                :auto-upload="false" :on-change="handleOnMainChange" :file-list="mainFileList">
+                <el-button slot="trigger" size="small" type="primary">选取主图</el-button>
+                <el-button size="small" type="success" @click="handleUploadMainClick">上传</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件</div>
+            </el-upload>
+            上传后主图：<el-link type="primary">{{mainPicUrl}}</el-link><br>
+            <el-image v-show="mainPicUrl" style="width: 100px; height: 100px" :src="mainPicUrl" fit="fill"></el-image>
             <br>
-        </div>
-        <br>
-        <br>
-
-        <el-button type="primary" @click="handleCreateClick">创建</el-button>
-
+            <br>
+    
+            <el-upload ref="uploadOther" multiple action="" :http-request="uploadOtherImage" :limit="9" accept="image/*"
+                :auto-upload="false" :on-change="handleOnOtherChange" :on-remove="handleOnOtherRemove"
+                :file-list="otherFileList">
+                <el-button slot="trigger" size="small" type="primary">选取其他图片</el-button>
+                <el-button size="small" type="success" @click="handleUploadOtherClick">上传</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件</div>
+            </el-upload>
+    
+            <br>
+            上传后其他图片：
+            <div v-for="item in otherPicUrls">
+                <el-link type="primary">{{item}}</el-link><br>
+                <el-image style="width: 60px; height: 60px" :src="item" fit="fill"></el-image>
+                <br>
+            </div>
+        </el-tab-pane>
+    </el-tabs>
     </div>
     `,
     data() {
         return {
+            activeTab: 'generalTab',
             productCode: '',
             productName: '',
             price: '',
@@ -84,6 +85,9 @@ const ProductCreateRoutePage = {
         });
     },
     methods: {
+        handleTabClick(tab, event) {
+            console.log('tab click', tab, event);
+        },
         handleGoBack() {
             console.log('go back click');
             this.$router.back();
