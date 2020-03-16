e1646ac3fea887548cb6e99336b58747ec898257
chenjiefei <nlpx_dc@hotmail.com>
2020-03-16 17:12:40 +0800

    fix test case with mockito

--- jcart-administration-back/src/test/java/io/cjf/jcartadministrationback/controller/AdministratorControllerTest.java
+++ jcart-administration-back/src/test/java/io/cjf/jcartadministrationback/controller/AdministratorControllerTest.java
@@ -4,11 +4,18 @@ import io.cjf.jcartadministrationback.constant.ClientExceptionConstant;
 import io.cjf.jcartadministrationback.dto.in.AdministratorLoginInDTO;
 import io.cjf.jcartadministrationback.dto.out.AdministratorLoginOutDTO;
 import io.cjf.jcartadministrationback.exception.ClientException;
+import io.cjf.jcartadministrationback.po.Administrator;
+import io.cjf.jcartadministrationback.service.AdministratorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class AdministratorControllerTest {
@@ -16,11 +23,18 @@ class AdministratorControllerTest {
     @Autowired
     private AdministratorController administratorController;
 
+    @MockBean
+    private AdministratorService administratorService;
+
     @Test
     void loginSuccess() throws ClientException {
         AdministratorLoginInDTO administratorLoginInDTO = new AdministratorLoginInDTO();
         administratorLoginInDTO.setUsername("admin");
         administratorLoginInDTO.setPassword("123456");
+        Administrator mockAdministrator = new Administrator();
+        mockAdministrator.setUsername("admin");
+        mockAdministrator.setEncryptedPassword("$2a$12$LCQseGaU5sCdBFX98Kid.OkOvcnUYYswQrbU732Y8kN.UbWDiEua.");
+        when(administratorService.getByUsername("admin")).thenReturn(mockAdministrator);
         AdministratorLoginOutDTO loginOutDTO = administratorController.login(administratorLoginInDTO);
         assertNotNull(loginOutDTO);
 
