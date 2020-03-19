e85ec667180c2985d847f5f74e14440139ba09d6
chenjiefei <nlpx_dc@hotmail.com>
2020-03-19 10:41:55 +0800

    add count down

--- jcart-administration-front/viewmodels/administrator-forget-pwd.js
+++ jcart-administration-front/viewmodels/administrator-forget-pwd.js
@@ -2,12 +2,25 @@ var app = new Vue({
     el: '#app',
     data: {
         email: '',
-        loading: false
+        loading: false,
+        buttonEnabled: true,
+        counter: 60
     },
     methods: {
         handleFindBackPwdClick() {
             console.log('find back pwd click');
             this.loading = true;
+            this.buttonEnabled = false;
+            this.counter = 60;
+
+            setInterval(function () {
+                console.log('count down');
+                app.counter--;
+                if (app.counter < 0) {
+                    app.buttonEnabled = true;
+                }
+            }, 1000);
+
             this.getPwdResetCode();
         },
         getPwdResetCode() {
@@ -19,7 +32,7 @@ var app = new Vue({
                 .then(function (response) {
                     console.log(response);
                     app.loading = false;
-                    alert('重置码已发送到邮箱');
+                    // alert('重置码已发送到邮箱');
                 })
                 .catch(function (error) {
                     console.log(error);
