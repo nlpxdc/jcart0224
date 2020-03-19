cfa369070ef50b6e7564f01df83c5d2ac733db54
chenjiefei <nlpx_dc@hotmail.com>
2020-03-19 09:37:15 +0800

    fix administration administrator forget pwd with loading

--- jcart-administration-front/viewmodels/administrator-forget-pwd.js
+++ jcart-administration-front/viewmodels/administrator-forget-pwd.js
@@ -1,11 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-        email: ''
+        email: '',
+        loading: false
     },
     methods: {
         handleFindBackPwdClick() {
             console.log('find back pwd click');
+            this.loading = true;
             this.getPwdResetCode();
         },
         getPwdResetCode() {
@@ -16,6 +18,7 @@ var app = new Vue({
             })
                 .then(function (response) {
                     console.log(response);
+                    app.loading = false;
                     alert('重置码已发送到邮箱');
                 })
                 .catch(function (error) {
