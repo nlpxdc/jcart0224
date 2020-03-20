2db67571c988c846682d85422bc8bb80ccd4a1f6
chenjiefei <nlpx_dc@hotmail.com>
2020-03-20 10:08:01 +0800

    first init support service

---
+++ jcart-support-back/src/main/java/io/cjf/jcartsupportback/JcartSupportBackApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jcartsupportback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartSupportBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartSupportBackApplication.class, args);
+    }
+
+}
---
+++ jcart-support-back/src/test/java/io/cjf/jcartsupportback/JcartSupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jcartsupportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartSupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
